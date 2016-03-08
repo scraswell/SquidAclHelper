9f88f2f49feb4120de1d4e9d1e0c32de69289b5f
Sean Craswell <scraswell@gmail.com>
2016-03-07 21:40:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Craswell.Squid.AclHelper;
+using log4net;
+using log4net.Config;
+
+namespace SquidAclHelper
+{
+    /// <summary>
+    /// The main class for the CLI program.
+    /// </summary>
+    internal class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            ILog logger = LogManager.GetLogger("SquidAclHelper");
+            var helper = new Craswell.Squid.AclHelper.SquidAclHelper(logger);
+
+            helper.Start();
+        }
+    }
+}
